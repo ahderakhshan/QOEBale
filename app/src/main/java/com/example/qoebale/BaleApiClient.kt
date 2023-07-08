cfa1e5a4f0513cd6998f6e9fde310e6cac63a6a6
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2023-07-08 22:28:23 +0430

    v0.0.1 project

--- app/src/main/java/com/example/qoebale/BaleApiClient.kt
+++ app/src/main/java/com/example/qoebale/BaleApiClient.kt
@@ -1,6 +1,7 @@
 package com.example.qoebale
 
 import io.ktor.client.*
+import io.ktor.client.engine.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.features.json.*
@@ -14,6 +15,14 @@ import kotlinx.serialization.Serializable
 import java.io.File
 import kotlin.system.measureTimeMillis
 import io.ktor.http.Parameters
+import io.ktor.utils.io.ByteChannel
+import java.io.BufferedInputStream
+import java.net.URL
+import java.net.URLConnection
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+
 
 @Serializable
 data class SendMessageRequest(
@@ -44,11 +53,11 @@ class BaleApiClient {
             serializer = KotlinxSerializer()
         }
     }
-    val clientphoto = HttpClient(CIO)
+    val clientfile = HttpClient(CIO)
 
 
 
-    suspend fun sendMessage(chatId: String, text: String): Float {
+    suspend fun sendMessage(chatId: String, text: String): Double {
         val requestData = SendMessageRequest(
             chat_id = chatId,
             text = text
@@ -63,18 +72,90 @@ class BaleApiClient {
             }
         }
         val average_time = 10*5*1000 / time
-        return average_time.toFloat()
+        return average_time.toDouble()
 
     }
-    suspend fun SendPhoto(){
+    suspend fun SendPhoto(): Double {
+        val time = measureTimeMillis {
+            val r: HttpResponse = clientfile.submitForm(
+                url = "https://tapi.bale.ai/bot1418026098:BwdEO97xAdUaIb86XyNDigImRVhQM16lFoo8M8Al/SendPhoto",
+                formParameters = Parameters.build{
+                    append("chat_id","@mabahesv2")
+                    append("photo","https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
+                }
 
-        val r: HttpResponse = clientphoto.submitForm(
-            url = "https://tapi.bale.ai/bot1418026098:BwdEO97xAdUaIb86XyNDigImRVhQM16lFoo8M8Al/SendPhoto",
-            formParameters = Parameters.build{
-                append("chat_id","@mabahesv2")
-                append("photo","https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
-            }
+            )
+        }
+        val result = (5969 * 1000)/time // size of image is 5969 byte
+        return result.toDouble()
+    }
+    suspend fun sendAudio(): Double {
+        val time = measureTimeMillis {
+            val r: HttpResponse = clientfile.submitForm(
+                url = "https://tapi.bale.ai/bot1418026098:BwdEO97xAdUaIb86XyNDigImRVhQM16lFoo8M8Al/Sendaudio",
+                formParameters = Parameters.build {
+                    append("chat_id", "@mabahesv2")
+                    append(
+                        "audio",
+                        "https://dls.music-fa.com/tagdl/1401/Amir%20Kermanshahi%20-%20Mashhadia%20Tehronia%20(320).mp3"
+                    )
+                }
+            )
+        }
+        val result = (8.58*1000*1000000)/time //size of audio is 8.58 mb
+        return result.toDouble()
+    }
+    suspend fun sendDoc(): Double {
+        val time = measureTimeMillis {
+            val r: HttpResponse = clientfile.submitForm(
+                url = "https://tapi.bale.ai/bot1418026098:BwdEO97xAdUaIb86XyNDigImRVhQM16lFoo8M8Al/Senddocument",
+                formParameters = Parameters.build {
+                    append("chat_id", "@mabahesv2")
+                    append(
+                        "document",
+                        "https://drive.google.com/file/d/1q0arRJkExGePWCyS0N1dz6DhmOv2Lb-5/view?usp=sharing"
+                    )
+                }
+            )
+        }
+        val result = (78.46*1000*1000)/(time)
+        return result.toDouble()
+    }
+    suspend fun sendVideo(): Double {
+        val time = measureTimeMillis {
+            clientfile.submitForm (
+                url = "https://tapi.bale.ai/bot1418026098:BwdEO97xAdUaIb86XyNDigImRVhQM16lFoo8M8Al/Sendvideo",
+                formParameters = Parameters.build {
+                    append("chat_id", "@mabahesv2")
+                    append("video",
+                        "https://aspb1.asset.aparat.com/aparat-video/4ae9be770d355ea038af40407364d41411919097-144p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjY0MDNmMmRmMjMxZmI3MzQwMGM0MTFkMmRmMjgxNzVkIiwiZXhwIjoxNjg4NjY2MjcyLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.7WOKbKrzlxbLnih56uu4WHHv_-PZ1p_t4Nn4IGpnlHE")
+                }
 
-        )
+            )
+        }
+        val result = ( 13940 * 1000 * 1000) / time // 13940 kbyte is size of video
+        return result.toDouble()
+    }
+    suspend fun getAdminsInfo(): Double {
+        val obj = URL("https://tapi.bale.ai/bot1418026098:BwdEO97xAdUaIb86XyNDigImRVhQM16lFoo8M8Al/getChatAdministrators?chat_id=@mabahesv2")
+        val connection = obj.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        val time = measureTimeMillis {
+            connection.responseCode
+        }
+        connection.disconnect()
+        val result = 483 * 1000 / time
+        return result.toDouble()
+    }
+    suspend fun getFileInfo(): Double{
+        val obj = URL("https://tapi.bale.ai/bot1418026098:BwdEO97xAdUaIb86XyNDigImRVhQM16lFoo8M8Al/getfile?file_id=1418026098:559251684355350273:1:c3f6fd3a43f364514be600e821b55abe1ef92213e01ce0169c3e2e6ac98129abc4e668c45e70295222fb0d6eece7e68cd554fc4790e9387a")
+        val connection = obj.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        val time = measureTimeMillis {
+            connection.responseCode
+        }
+        connection.disconnect()
+        val result = 729 * 1000 / time
+        return result.toDouble()
     }
 }
--- app/src/main/java/com/example/qoebale/MainActivity.kt
+++ app/src/main/java/com/example/qoebale/MainActivity.kt
@@ -1,47 +1,91 @@
 package com.example.qoebale
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CheckBox
 import android.widget.TextView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.io.IOError
+import android.os.Handler
+import android.os.Looper
+import com.example.chonto.MyDatabaseBuilder
+import com.example.chonto.QOEParams
+import com.example.chonto.saveUsersToDatabase
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     private val baleApiClient = BaleApiClient()
-
+    var text_send_speed = 0.0
+    var photo_send_speed = 0.0
+    var audio_send_speed = 0.0
+    var doc_send_speed = 0.0
+    var video_send_speed = 0.0
+    var get_admininfo_speed = 0.0
+    var get_fileinfo_speed = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
-        val sendphotobutton = findViewById<Button>(R.id.button2)
-        val checkbox = findViewById<CheckBox>(R.id.checkBox)
-        val text = findViewById<TextView>(R.id.textView)
-        button.setOnClickListener {
-            checkbox.isChecked = ! checkbox.isChecked
+        val startqoebutton = findViewById<Button>(R.id.button4)
+        val text = findViewById<TextView>(R.id.textView2)
+
+        startqoebutton.setOnClickListener {
+            text.text = "QOE started \n" +
+                    "waiting for results"
             GlobalScope.launch(Dispatchers.Main) {
-                val a = sendBaleMessage()
-                text.text = a.toString()
+                    text_send_speed = sendBaleMessage()
+                    photo_send_speed = sendBalePhoto()
+                    doc_send_speed = sendBaleDoc()
+                    audio_send_speed = sendBaleAudio()
+                    video_send_speed = sendBaleVideo()
+                    text.text = "text send speed : $text_send_speed \n" +
+                        "photo send speed :  $photo_send_speed \n" +
+                        "doc send speed : $doc_send_speed \n" +
+                        "audio_send_speed : $audio_send_speed\n " +
+                        "video send speed : $video_send_speed\n " +
+                        "get fileinfo speed : $get_fileinfo_speed\n" +
+                        "get admin info speed : $get_admininfo_speed\n" +
+                        "all of this numbers are byte per seconds"
+
             }
-
-        }
-        sendphotobutton.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main) {
-                sendBalePhoto()
+            GlobalScope.launch(Dispatchers.IO) {
+                    get_fileinfo_speed = getFileInfo()
+                    get_admininfo_speed = getAdminInfo()
 
             }
         }
     }
 
-    suspend fun sendBaleMessage(): Float {
-        val a = baleApiClient.sendMessage("@mabahesv2", "salam")
-        return a
+
+    suspend fun sendBaleMessage(): Double {
+        val result = baleApiClient.sendMessage("@mabahesv2", "salam")
+        return result
     }
-    suspend fun sendBalePhoto(){
-        baleApiClient.SendPhoto()
+    suspend fun sendBalePhoto(): Double{
+        val result = baleApiClient.SendPhoto()
+        return result
     }
+    suspend fun sendBaleAudio(): Double {
+        val result = baleApiClient.sendAudio()
+        return result
+    }
+    suspend fun sendBaleDoc(): Double {
+        val result = baleApiClient.sendDoc()
+        return result
+    }
+    suspend fun sendBaleVideo(): Double {
+        val result = baleApiClient.sendVideo()
+        return result
+    }
+    suspend fun getAdminInfo(): Double {
+        val result = baleApiClient.getAdminsInfo()
+        return result
+    }
+    suspend fun getFileInfo(): Double {
+        return baleApiClient.getFileInfo()
+    }
+
 }
